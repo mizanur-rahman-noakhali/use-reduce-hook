6896ca7abc23cf03af70c2e0a323f1e6cb4918b3
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-10-13 00:12:22 +0600

    first commit

--- src/components/PatientManagement/PatientManagement.js
+++ src/components/PatientManagement/PatientManagement.js
@@ -1,14 +1,33 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientState } from '../../reducers/patientReducers';
 
 const PatientManagement = () => {
+  const nameRef=useRef();
+
   const [state,dispatch]=useReducer(patientReducer,patientState);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch({
+    type:'ADD_PATIENT',
+    name:nameRef.current.value,
+    id:state.patients.length+1
+  })
+    nameRef.current.value='';
+  }
+
   return (
     <div>
       <h1>PatientManagement:{state.patients.length}</h1>
-      <form>
-        <input value></input>
+      <form onSubmit={handleSubmit}>
+        <input ref={nameRef}></input>
       </form>
+      {
+        state.patients.map(pt=><li
+           key={pt.id}
+            onClick={()=>dispatch({type:'REMOVE_PATIENT',id:pt.id})}>
+            {pt.name}</li>)
+      }
     </div>
   );
 };
