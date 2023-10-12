f60f7ecb05e7bdebebfd28eba0662f52045b055e
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-10-12 20:28:44 +0600

    reduce

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ReduceCount from './components/ReduceCount/ReduceCount';
 
 function App() {
   return (
     <div className="App">
-    
+    <ReduceCount></ReduceCount>
     </div>
   );
 }
